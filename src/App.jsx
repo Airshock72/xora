d2c0edf285bde85ee9bf66a3f1cede96d06a8e6e
Airshock72 <gelagela2222@gmail.com>
2024-11-15 01:30:11 +0400

    implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './sections/Header.jsx'
+import Hero from './sections/Hero.jsx'
 import React from 'react'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header />
+      <Hero />
     </main>
   )
 }
