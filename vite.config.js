ec732707ec650961085f20d47b5db4f03be10eb8
Airshock72 <gelagela2222@gmail.com>
2024-12-05 20:26:09 +0400

    modified vite config and package json script files

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/xora/',
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase to a higher KB limit
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          pixi: ['pixi.js'],
-          gsap: ['gsap'],
-        },
-      },
-    },
-  },
+  plugins: [react()]
 })
